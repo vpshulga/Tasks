0b19be0ba6a3146133d9e9cba33cd29bfaab6544
vpshulga <vpshulgaa@gmail.com>
2018-06-25 14:36:01 +0300

    Task21_Complete

---
+++ src/by/vpshulga/lesson21/library_authors.sql
@@ -0,0 +1,6 @@
+INSERT INTO library.authors (name, surname, country) VALUES ('Sergey', 'Esenin', 'Russia');
+INSERT INTO library.authors (name, surname, country) VALUES ('Alex', 'Pushkin', 'Russia');
+INSERT INTO library.authors (name, surname, country) VALUES ('Ray', 'Bradbury', 'USA');
+INSERT INTO library.authors (name, surname, country) VALUES ('Jules', 'Verne', 'France');
+INSERT INTO library.authors (name, surname, country) VALUES ('Jakub', 'Kolas', 'Belarus');
+INSERT INTO library.authors (name, surname, country) VALUES ('William', 'Shakespeare', 'England');
